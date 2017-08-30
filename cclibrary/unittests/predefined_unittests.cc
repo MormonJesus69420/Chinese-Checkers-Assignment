37d427701f915935bd5970a6727c3ae4c39e926c
Jostein Bratlie <jostein.bratlie@gmail.com>
2017-08-30 20:55:59 +0200

    Added three predefined unittests checking initial board goal and piece sets after initialization."

--- cclibrary/unittests/predefined_unittests.cc
+++ cclibrary/unittests/predefined_unittests.cc
@@ -1,12 +1,136 @@
 // gtest
-#include <gtest/gtest.h> // googletest header file
+#include <gtest/gtest.h>   // googletest header file
 
 // cc library
 #include "../cclibrary.h"
 
 using CustomGameClass = MySoothingNamespace::Game;
 
-TEST (PredefinedUnittests, HelloWorld)
+TEST(PredefinedUnittests, BoardInitialization_TwoPlayerGame_PlayerAndGoalPieces)
 {
   CustomGameClass game;
+  game.initNewGame(CC::NoPlayers::Two);
+
+  // Player one
+  EXPECT_EQ(game.pieces(CC::PlayerId::One),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+  EXPECT_EQ(
+    game.goal(CC::PlayerId::One),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+
+  // Player two
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Two),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+  EXPECT_EQ(game.goal(CC::PlayerId::Two),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+}
+
+TEST(PredefinedUnittests, BoardInitialization_ThreePlayerGame_PlayerAndGoalPieces)
+{
+  CustomGameClass game;
+  game.initNewGame(CC::NoPlayers::Three);
+
+  // Player one
+  EXPECT_EQ(game.pieces(CC::PlayerId::One),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+  EXPECT_EQ(
+    game.goal(CC::PlayerId::One),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+
+  // Player two
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Two),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
+  EXPECT_EQ(game.goal(CC::PlayerId::Two),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
+
+  // Player three
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Three),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).first);
+  EXPECT_EQ(game.goal(CC::PlayerId::Three),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).second);
+}
+
+TEST(PredefinedUnittests, BoardInitialization_FourPlayerGame_PlayerAndGoalPieces)
+{
+  CustomGameClass game;
+  game.initNewGame(CC::NoPlayers::Four);
+
+  // Player one
+  EXPECT_EQ(game.pieces(CC::PlayerId::One),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+  EXPECT_EQ(
+    game.goal(CC::PlayerId::One),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+
+  // Player two
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Two),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+  EXPECT_EQ(game.goal(CC::PlayerId::Two),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+
+  // Player three
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Three),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
+  EXPECT_EQ(game.goal(CC::PlayerId::Three),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
+
+  // Player four
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Four),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
+  EXPECT_EQ(game.goal(CC::PlayerId::Four),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
+}
+
+TEST(PredefinedUnittests, BoardInitialization_SixPlayerGame_PlayerAndGoalPieces)
+{
+  CustomGameClass game;
+  game.initNewGame(CC::NoPlayers::Six);
+
+  // Player one
+  EXPECT_EQ(game.pieces(CC::PlayerId::One),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+  EXPECT_EQ(
+    game.goal(CC::PlayerId::One),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+
+  // Player two
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Two),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
+  EXPECT_EQ(game.goal(CC::PlayerId::Two),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
+
+  // Player three
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Three),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).first);
+  EXPECT_EQ(game.goal(CC::PlayerId::Three),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).second);
+
+  // Player four
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Four),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).second);
+  EXPECT_EQ(game.goal(CC::PlayerId::Four),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::One)).first);
+
+  // Player five
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Five),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
+  EXPECT_EQ(game.goal(CC::PlayerId::Five),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
+
+  // Player six
+  EXPECT_EQ(
+    game.pieces(CC::PlayerId::Six),
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).second);
+  EXPECT_EQ(game.goal(CC::PlayerId::Six),
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).first);
 }

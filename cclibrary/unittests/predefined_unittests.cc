84ece922200d9d75d7b9d712b1be92a8b1948fe7
Jostein Bratlie <jostein.bratlie@gmail.com>
2017-08-31 13:05:20 +0200

    Fixed to three player game init positions unit tests.

--- cclibrary/unittests/predefined_unittests.cc
+++ cclibrary/unittests/predefined_unittests.cc
@@ -46,16 +46,16 @@ TEST(PredefinedUnittests, BoardInitialization_ThreePlayerGame_PlayerAndGoalPiece
   // Player two
   EXPECT_EQ(
     game.pieces(CC::PlayerId::Two),
-    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).first);
   EXPECT_EQ(game.goal(CC::PlayerId::Two),
-            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).second);
 
   // Player three
   EXPECT_EQ(
     game.pieces(CC::PlayerId::Three),
-    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).first);
+    CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).second);
   EXPECT_EQ(game.goal(CC::PlayerId::Three),
-            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Three)).second);
+            CustomGameClass::pieceSets().at(size_t(CC::PieceSetId::Two)).first);
 }
 
 TEST(PredefinedUnittests, BoardInitialization_FourPlayerGame_PlayerAndGoalPieces)
